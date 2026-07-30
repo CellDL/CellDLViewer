116e56b92e4870dac76406468501eb06aff92c4c
David Brooks <dbrnz@users.noreply.github.com>
2026-07-30 16:21:56 +1200

    Rework `rollup` packaging (based on that used for the CellDL Editor's component).

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,4 +1,5 @@
 import * as primeVueAutoImportResolver from '@primevue/auto-import-resolver'
+import typescript from '@rollup/plugin-typescript'
 import tailwindcssPlugin from '@tailwindcss/vite'
 import vuePlugin from '@vitejs/plugin-vue'
 
@@ -23,39 +24,26 @@ export default vite.defineConfig({
         rollupOptions: {
             external: ['vue'],
             output: {
+                dir: 'dist',
                 exports: 'named',
                 globals: {
                     vue: 'Vue'
                 },
-                assetFileNames: (assetInfo: { names: string }) => {
-                    if (assetInfo.names.includes('celldl-editor.css')) {
+                assetFileNames: (assetInfo: { names: string[] }) => {
+                    if (assetInfo.names.includes('viewer.css')) {
                         return 'CellDLViewer.css'
                     }
-
                     return assetInfo.names[0] ?? 'default-name'
-                },
-                manualChunks: (id: string) => {
-                    // List of modules that rollup sometimes bundles with manual chunks
-                    // causing those chunks to be eager-loaded
-                    const ROLLUP_COMMON_MODULES = [
-                        'vite/preload-helper',
-                        'vite/modulepreload-polyfill',
-                        'vite/dynamic-import-helper',
-                        'commonjsHelpers',
-                        'commonjs-dynamic-modules',
-                        '__vite-browser-external'
-                    ];
-
-                    // Bundle all 3rd-party modules into a single vendor.js module
-                    if (id.includes('node_modules')
-                     || ROLLUP_COMMON_MODULES.some((commonModule) => id.includes(commonModule))) {
-                        return 'vendor';
-                    }
-
-                    // All other files are project source files and are allowed
-                    // to be split whichever way rollup wants
                 }
-            }
+            },
+            plugins: [
+                typescript({
+                    include: [
+                        './index.ts',
+                        'src/**'
+                    ]
+                }),
+            ]
         },
         sourcemap: true,
         target: 'esnext'
