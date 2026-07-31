51a882cb5603b4f954812b6d92d945db407911e2
David Brooks <dbrnz@users.noreply.github.com>
2026-07-31 16:19:48 +1200

    Try renaming the bundled component to prevent recursive lookup when using the `@celldl/viewer` package.

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ limitations under the License.
 
 ******************************************************************************/
 
-import CellDLViewer from './components/CellDLViewer.vue'
+import CellDLViewer from './components/ViewerComponent.vue'
 
 //==============================================================================
 
